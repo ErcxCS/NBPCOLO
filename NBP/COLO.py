f9e8444de303d8915272bc72d525747a339d85eb
ErcxCS <berkerciks@gmail.com>
2023-12-23 20:13:34 +0300

    good results

--- NBP/COLO.py
+++ NBP/COLO.py
@@ -3,6 +3,17 @@ from sklearn.metrics import euclidean_distances
 from matplotlib import pyplot as plt
 from scipy.stats import norm, gaussian_kde
 
+
+def rmse(X, X_hat_ab):
+    # X is the true coordinates matrix
+    # X_hat_ab is the estimated coordinates matrix
+
+    # Compute the Euclidean distance for each node
+    error = np.sqrt(np.sum((X - X_hat_ab)**2, axis=1))
+    error2 = np.linalg.norm(X - X_hat_ab)
+    # Compute the RMSE
+    return np.sqrt(np.mean(error**2))
+
 def mono_potential_bbox(bbox: np.ndarray):
     """
     Returns a function of prior probability for a given bounded box.
@@ -223,64 +234,137 @@ def iterative_NBP(D: np.ndarray, X: np.ndarray, anchors: np.ndarray,
     n_targets = n_samples - n_anchors # number of nodes to localize
     anchor_list = list(range(n_anchors)) # first n nodes are the anchors
 
-    target_neighbours = n_samples - n_anchors - 1 # Assumes fully connected graph, find a way for not fully connected
+    target_neighbours = n_samples - 1 # Assumes fully connected graph, find a way for not fully connected
+    # target_neighbours = n_samples - n_anchors - 1
     new_n_particles = round(k * n_particles / target_neighbours)
     n_particles = round(new_n_particles * target_neighbours / k)
 
     intersections = create_bbox(D, anchors, limits=deployment_area)
     M, prior_beliefs = generate_particles(intersections, anchors, n_particles)
 
-    """ plt.scatter(X[:, 0], X[:, 1])
-    for i, bbox in enumerate(intersections):
-        xmin, xmax, ymin, ymax = bbox
-        plt.plot([xmin, xmax, xmax, xmin, xmin], [ymin, ymin, ymax, ymax, ymin], c='red')
-        plt.annotate(f"a{i}", X[i])
-    plt.show() """
     messages = np.ones((n_samples, n_samples, n_particles))
     new_messages = messages.copy()
     weights = prior_beliefs / np.sum(prior_beliefs, axis=1, keepdims=True)
+    # we have different prior beliefs for each node, however we do not use them
+    # anywhere, we normalize weights, because all priors are the same, all weights
+    # for all particles of all nodes will be the same.
 
     for iter in range(n_iter):
-        M_new = np.ones((n_samples, n_samples, new_n_particles, 2))
+        M_new = np.ones((n_samples, n_samples, new_n_particles, d_dim))
         m_ru = dict()
         
         for r, Mr in enumerate(M):
-            for u, Mu in enumerate(M):
+            # sender
+            for u, Mu in enumerate(M): # skip anchors?
+                # receiver
                 if r != u:
                     v = np.random.normal(0, 2, size=n_particles)*0
                     thetas = np.random.uniform(0, 2*np.pi, size=n_particles)
                     cos_u = (D[r, u] + v) * np.cos(thetas)
                     sin_u = (D[r, u] + v) * np.sin(thetas)
-                    x_ru = Mr + np.column_stack([cos_u, sin_u])
-
-                    qq = mono_potential_bbox(intersections[u])(x_ru)
-                    
+                    x_ru = Mr + np.column_stack([cos_u, sin_u])             
 
                     w_ru = weights[r] / messages[r, u]
                     w_ru /= w_ru.sum()
-                    w_ru *= qq
-                    
-                    print(w_ru)
 
                     kde = gaussian_kde(x_ru.T, weights=w_ru, bw_method='silverman')
                     m_ru[r, u] = kde
+                    # kde constructed with particles of r to evaluate resampled particles of u
                     M_new[u, r] = kde.resample(new_n_particles).T
+                    # particles of u generated from neighbour node r's kde
+                    # We shouldn't generate new particles for anchors?
+                    # If we don't generate particles for achors, 
+                    # which particles are gonna use to evaluate them?
+                    """if u in anchor_list:
+                        M_new[u, r] = Mu """ # Will cause shape miss match
+                    # All Mu particles are on top of each other
+                    # Should we use x_ur instead?
+                    # x_ur is m_ru[u, r].dataset() which we may not have atm
+                    # This kinda does makes sense and inline with both papers.
+                    # However, this may also mean T0 is non anchor neighbours and not observed neighbours
+                    # So, T1 is not multi-hop neighbours, its just neighbours
+                    # Still, this doesn't contradicts with the neighnours are n-hop neighbours
+                    #   r: sender
+                    #   u: receiver (above problem occurs when u is anchor)
+                    # We still need x_ru for anchors because they send messages
+        
+        mask = np.all(M_new == np.ones((new_n_particles, d_dim)), axis=(2, 3))
+        M_new = M_new[~mask]
+        M_new = M_new.reshape(n_samples, k*n_particles, d_dim)
+        # migh need to change to (n_samples, k*new_n_particles*(n_samples-1), d_dim)
 
+        for u, Mu_new in enumerate(M_new): # skip anchors?
+            # receiver
+            for v, Mv_new in enumerate(M_new):
+                # sender
+                if u != v:
+                    m_vu = m_ru[v, u](Mu_new.T) # Mu_new is the sampled particles from u's neighbour kde's
+                    # m_vu is messages of particles from v to u
+                    # for anchors sending messages this is fine
+                    # however, when u is anchor maybe we should use
+                    # m_ru[u, v].dataset()? instead of Mu_new.T
+                    # if using above, then keep the particles of anchors as it is in the end:
+                    #   M_new[:n_anchors] = M[:n_anchors]
+                    #   Do we have to? particles of target nodes u will change in every iteration
+                    #   So will their kde. But distance from u to v is measured distance, so it will
+                    #   Stay the same, so, when u is anchor, its kde for all neighbours v shouldn't change?
+                    #   Just keep m_ru[u, v]? Unnecessary, just reconstruct anchor kdes each time.
+                    # m_ru[v, u] is the kde constructed with particles of v (r == v) with distance to u
+                    new_messages[u, v] = m_vu # message that u receives from v
+           
+            proposal_product_u = np.prod(new_messages[u, [i for i in range(n_samples) if i != u]], axis=0)
+            proposal_sum_u = np.sum(new_messages[u, [i for i in range(n_samples) if i != u]], axis=0)
+            # This doesn't makes sense...?
+            # Maybe, apply following changes when using m_ru[u, v].dataset() instead of Mu_new.T?
+            # TODO: change target_neighbours, change M_new.reshape, change proposal_sum
+            # T0 should be non anchor neighbours
+            # proposal_sum_u = np.sum(new_messages[u, [idx for idx in list(range(n_samples)) if idx not in (anchor_list + [u])]], axis=0)
+            W_u = proposal_product_u / proposal_sum_u
+            W_u /= W_u.sum()
 
-np.random.seed(23)
-n, d = 8, 2
-m = 25
-p = 48
+            idxs = np.arange(n_particles)
+            indices = np.random.choice(idxs, size=n_particles, replace=True, p=W_u)
+
+            M_new[u] = Mu_new[indices]
+            #weights[u] = W_u#[indices] #???
+            messages[u] = new_messages[u, :, indices].T
+
+        M = M_new
+
+        weighted_means = np.einsum('ijk,ij->ik', M, weights)
+        print(rmse(X, weighted_means))
+        plt.scatter(X[n_anchors:, 0], X[n_anchors:, 1], color='c')
+        plt.scatter(X[:n_anchors, 0], X[:n_anchors, 1], color='m')
+        plt.scatter(weighted_means[:, 0], weighted_means[:, 1], color='y')
+        for i, bbox in enumerate(intersections):
+            if i >= n_anchors:
+                xmin, xmax, ymin, ymax = bbox
+                plt.plot([xmin, xmax, xmax, xmin, xmin], [ymin, ymin, ymax, ymax, ymin], label=f"{i}")
+                plt.annotate(f"Xt{i}", X[i])
+                plt.annotate(f"Pt{i}", weighted_means[i])
+            else:
+                plt.annotate(f"A{i}", X[i])
+                plt.annotate(f"Pa{i}", weighted_means[i])
+        plt.legend()
+        plt.show()
+
+        
+
+#np.random.seed(21)
+n, d = 16, 2
+m = 50
+p = 300
 r = 15
-a = 4
-i = 5
+a = 8
+i = 10
+k = 1
 X = np.random.uniform(0, m, size=(n, d))
 
 noise = np.random.normal(0, 2, size=(n, n))
 noise -= np.diag(noise.diagonal())
 symetric_noise = (noise + noise.T) / 2
 D = euclidean_distances(X) + symetric_noise*0
-iterative_NBP(D=D, X=X, anchors=X[:a], deployment_area=np.array([0, m, 0, m]), n_particles=p, n_iter=i, k=1)
+iterative_NBP(D=D, X=X, anchors=X[:a], deployment_area=np.array([0, m, 0, m]), n_particles=p, n_iter=i, k=k)
 
 
                     
